072f0e8fcd8be776f438cb56886ba3329f188061
Adam Riggs <phlux1111@gmail.com>
2015-12-28 18:03:19 -0500

    added additional comments

--- js/InstagramApp.js
+++ js/InstagramApp.js
@@ -4,6 +4,7 @@ function InstagramApp() {
 	console.log("InstagramApp()");
 
 	//next variables hold the html elements in a jquery wrapper
+	//when we have these elements as javascript variables we have much more control over them and can do things like make them scroll like a carousel
 	this.$carousel = $(".carousel");
 	this.$carouselImgs = $(".carousel-img");
 
