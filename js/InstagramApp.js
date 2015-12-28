66c6f2e127fc1f57a248a90ba7f5e63fd766d93f
Adam Riggs <phlux1111@gmail.com>
2015-12-28 18:00:09 -0500

    added js code to access the carousel and the carousel images0

--- js/InstagramApp.js
+++ js/InstagramApp.js
@@ -1,7 +1,14 @@
 'use strict'
 
 function InstagramApp() {
-	console.log("InstagramApp");
+	console.log("InstagramApp()");
 
-	
+	//next variables hold the html elements in a jquery wrapper
+	this.$carousel = $(".carousel");
+	this.$carouselImgs = $(".carousel-img");
+
+
+	//see them in the console window
+	console.log("carousel==", this.$carousel);
+	console.log("carousel images ==", this.$carouselImgs);
 };
