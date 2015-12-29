e0d6474c9757df459c3244d1ba8926d26addfedc
Adam Riggs <phlux1111@gmail.com>
2015-12-29 11:43:32 -0500

    added some comments and set the width of carousel-img items with js

--- js/Carousel.js
+++ js/Carousel.js
@@ -6,7 +6,7 @@ function Carousel() {
 	//next variables hold the html elements in a jquery wrapper
 	//when we have these elements as javascript variables we have much more control over them and can do things like make them scroll like a carousel
 	this.$carousel = $(".carousel");	//the $ is indicative of jquery which smooths out the differences in how browsers handle javascript
-	this.$carouselImgs = $(".carousel-img");
+	this.$carouselImgs = $(".carousel-img");	//since there are many things with the class carousel-img this variable is an array.  Actually they all are but if the length is 0 its treated like its not an array
 
 
 	//see them in the console window
@@ -17,7 +17,10 @@ function Carousel() {
 
 	//for every carousel-img create a new js object and pass the html element into it
 	for(var i = 0; i < this.$carouselImgs.length; i++){
-		this.carouselImgs.push( new CarouselItem(this.$carouselImgs[i]));
+
+		//sometimes when you deal with arrays of jquery objects they stop being jquery objects and they are simple html DOM elements
+		//I then re-wrap them in the $ so they become jquery objects again.  THEN I pass them into the CarouselItem object
+		this.carouselImgs.push( new CarouselItem($(this.$carouselImgs[i])));
 	}
 	
 };
--- js/CarouselItem.js
+++ js/CarouselItem.js
@@ -6,6 +6,9 @@ function CarouselItem(El) {
 	//I like to do this sometimes to make sure that the data is passed into the object correctly
 	console.log(this.$el);
 
-	
+	//You can have dymanic control of css properties like width or... position - which is what we will be using to scroll the carousel.
+	//we dont' really need to set the width here.  I just wanted to illustrate that you could.
+	//using straight css is actually faster and more reliable.  You should only control the css with javascript when you need to.
+	this.$el.css({width: '300px'});
 
 };
